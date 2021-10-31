24b943ffbf5384e081ac86610ad4453a3cf723a9
nogw <gabrielnogueiraoliveira@gmail.com>
2021-10-31 01:00:36 -0300

    starting to implement the proof of work

--- lib/blockchain.ml
+++ lib/blockchain.ml
@@ -1,5 +1,3 @@
-open Utils
-
 type timestamp = [%import: Unix.tm] [@@deriving yojson]
 
 type block = 
@@ -8,29 +6,72 @@ type block =
     hash: string;
     timestamp: timestamp;
     data: string;
+    difficulty: int;
+    nonce: int;
   }
   | Block of {
     index: int;
     hash: string;
     timestamp: timestamp;
     data: string;
+    difficulty: int;
+    nonce: int;
     previous_block: block 
   }
   [@@deriving yojson]
 
-let block index hash timestamp data previous_block =
+let index_of block =
+  match block with
+  | GenesisBlock b -> b.index
+  | Block b -> b.index
+
+let hash_of block =
+  match block with
+  | GenesisBlock b -> b.hash
+  | Block b -> b.hash
+
+let timestamp_of block =
+  match block with
+  | GenesisBlock b -> b.timestamp
+  | Block b -> b.timestamp
+
+let data_of block =
+  match block with
+  | GenesisBlock b -> b.data
+  | Block b -> b.data
+
+let difficulty_of block =
+  match block with
+  | GenesisBlock b -> b.difficulty
+  | Block b -> b.difficulty
+
+let nonce_of block =
+  match block with
+  | GenesisBlock b -> b.nonce
+  | Block b -> b.nonce
+
+let previous_of block =
+  match block with
+  | GenesisBlock _ -> failwith "Error: tried to obtain previous hash of genesis block"
+  | Block b -> b.previous_block
+
+let block index hash timestamp data difficulty nonce previous_block =
   Block {
     index;
     hash;
     timestamp;
     data;
+    difficulty;
+    nonce;
     previous_block;
   }
 
-let hash_block index timestamp ?(previous_hash = "") data =
+let hash_block index timestamp ?(previous_hash = "") data difficulty nonce =
   [ string_of_int index;
     Unix.mktime timestamp |> fst |> string_of_float;
     data;
+    string_of_int difficulty;
+    string_of_int nonce;
     previous_hash
   ] |> String.concat "" |> Sha256.string |> Sha256.to_hex
 
@@ -38,21 +79,29 @@ let initial_block data =
   let timestamp = Unix.time () |> Unix.gmtime in
   GenesisBlock {
     index = 0;
-    hash = hash_block 0 timestamp data;
+    hash = hash_block 0 timestamp data 0 0 ;
     timestamp;
     data;
+    difficulty = 0;
+    nonce = 0
   }  
 
-let add_next_block data previous_block = 
-  let timestamp = Unix.time () |> Unix.gmtime
-  and index = index_of previous_block + 1 in
-  Block {
-    index;
-    hash = hash_block index timestamp ~previous_hash:(hash_of previous_block) data;
-    timestamp;
-    data;
-    previous_block;
-  }
+let block_generation_interval = 10
+let difficulty_adjustment_interval = 10
+
+let get_adjusted_difficulty block previous_block = 
+  let prev_adjustment_block = Utils.get_block block (index_of block - difficulty_adjustment_interval) in
+  let time_expected = block_generation_interval * difficulty_adjustment_interval in
+  let time_taken = timestamp_of previous_block - timestamp_of prev_adjustment_block in
+  if time_taken < time_expected / 2 then difficulty_of prev_adjustment_block + 1
+  else if time_taken > time_expected * 2 then difficulty_of prev_adjustment_block - 1
+  else difficulty_of prev_adjustment_block
+
+let get_difficulty block =
+  let previous_block = previous_of block in
+  if (index_of previous_block mod difficulty_adjustment_interval = 0) && index_of previous_block != 0 then
+    get_adjusted_difficulty previous_block block
+  else difficulty_of previous_block
 
 let validate_next_block block =
   match block with
@@ -65,10 +114,25 @@ let validate_next_block block =
       | GenesisBlock pb -> pb.index
       | Block pb -> pb.index
     in
-      b.index = previous_index + 1 && hash = b.hash 
+      b.index = succ previous_index && hash = b.hash 
 
 let hash_matches_difficulty hash difficulty =
-  let hash_binary = hex_to_bin hash in
+  let hash_binary = Utils.hex_to_bin hash
+  and required_prefix = Utils.repeat "0" difficulty in
+  Utils.starts_with ~prefix: required_prefix hash_binary  
+
+let rec find_block index previous_block timestamp data difficulty =
+  let rec aux nonce =
+    let hash = hash_block index timestamp ~previous_hash:(hash_of previous_block) data difficulty nonce;
+    if hash_matches_difficulty hash difficulty then
+      block (index hash timestamp data difficulty nonce previous_block)
+    else aux (succ nonce)
+  in aux 0
+
+let add_next_block data previous_block = 
+  let timestamp = Unix.time () |> Unix.gmtime
+  and index = index_of previous_block + 1 in
+  find_block index previous_block timestamp data difficulty
 
 let validate_chain chain = 
   let rec aux chain' res = 
--- lib/utils.ml
+++ lib/utils.ml
@@ -1,20 +1,20 @@
-let bin_of_char c = 
+let bin_of_char c =
   match c with
-  | '0' -> "0000" | '1' -> "0001" | '2' -> "0010" | '3' -> "0011" 
-  | '4' -> "0100" | '5' -> "0101" | '6' -> "0110" | '7' -> "0111" 
-  | '8' -> "1000" | '9' -> "1001" | 'a' -> "1010" | 'b' -> "1011" 
-  | 'c' -> "1100" | 'd' -> "1101" | 'e' -> "1110" | 'f' -> "1111" 
+  | '0' -> "0000" | '1' -> "0001" | '2' -> "0010" | '3' -> "0011"
+  | '4' -> "0100" | '5' -> "0101" | '6' -> "0110" | '7' -> "0111"
+  | '8' -> "1000" | '9' -> "1001" | 'a' -> "1010" | 'b' -> "1011"
+  | 'c' -> "1100" | 'd' -> "1101" | 'e' -> "1110" | 'f' -> "1111"
   | _ -> failwith "hash error"
 
 let hex_to_bin s =
   let rec aux l i acc =
-    if i = l then acc 
+    if i = l then acc
     else aux l (succ i) (bin_of_char s.[i]) ^ acc
   in
     aux (String.length s) 0 "";;
 
-let rec repeat s n = 
-  if n = 0 then "" else s ^ string_repeat_1 s (n - 1)
+let rec repeat s n =
+  if n = 0 then "" else s ^ repeat s (n - 1)
 
 (* if i update to ocaml 4.13.^ dont need this *)
 let starts_with ~prefix s =
@@ -26,27 +26,9 @@ let starts_with ~prefix s =
     else aux (i + 1)
   in len_s >= len_pre && aux 0
 
-let index_of block =
-  match block with
-  | GenesisBlock b -> b.index
-  | Block b -> b.index
-
-let hash_of block =
-  match block with
-  | GenesisBlock b -> b.hash
-  | Block b -> b.hash
-
-let timestamp_of block =
-  match block with
-  | GenesisBlock b -> b.timestamp
-  | Block b -> b.timestamp
-
-let data_of block =
-  match block with
-  | GenesisBlock b -> b.data
-  | Block b -> b.data
-
-let previous_of block =
-  match block with
-  | GenesisBlock _ -> failwith "Error: tried to obtain previous hash of genesis block"
-  | Block b -> b.previous_block
+let get_block block n = 
+  let rec aux block' n' =
+    if n' = 0 then block'
+    else aux (Yojson.Safe.Util.member "previous_block" block' |> Yojson.Safe.Util.index 1) (n' - 1)
+  in
+    aux (Yojson.Safe.Util.index 1 block) n;;
